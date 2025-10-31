00f4157122576ce9bde5a3e1058618a512b6235b
Salman Althof <salmanalthof31@gmail.com>
2025-10-31 17:28:26 +0700

    feat: show deatil projects

--- src/components/projects/ProjectsCardList.tsx
+++ src/components/projects/ProjectsCardList.tsx
@@ -24,7 +24,6 @@ export default function ProjectsCardList ({ listProjects }: ProjectsCardListItf)
     }
 
     return(
-
         <div className="grid xl:grid-cols-[repeat(3,20rem)] 2xl:grid-cols-[repeat(3,25rem)] grid-cols-1 md:grid-cols-2 lg:grid-cols-[repeat(3,16rem)] gap-5 px-[4%] z-10 text-[#28031d]">
             {listProjects.slice().reverse().map((project) => (
                 <article onClick={() => handleRouter(project.id)} key={project.id} className="bg-gradient-to-br from-[#56c7e8] to-[#f62cd1] flex flex-col items-center justify-start rounded-[1rem] sm:w-[18rem] md:w-[20rem] lg:w-[16rem] xl:w-[20rem] 2xl:w-[25rem] transition duration-300 hover:drop-shadow-[0_0_12px_rgba(218,119,231,0.6)] active:scale-90">
